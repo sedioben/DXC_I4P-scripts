9f0f825d70251d3c5f76394c97dee79e2a60a881
Sedi Oben <sedioben@googlemail.com>
2017-08-19 16:05:58 +0200

    upload script

---
+++ shelladds.ps1
@@ -0,0 +1,31 @@
+#
+# Windows PowerShell script for AD DS Deployment
+#
+
+Param(
+   [Parameter(Mandatory=$True)]
+   [string]$DomainName,
+	
+   [Parameter(Mandatory=$True)]
+   [string]$DomainNetbiosName
+   
+   [Parameter(Mandatory=$True)]
+   [SecureString]$SafeModeAdministratorPassword
+)
+
+
+Install-WindowsFeature -name AD-Domain-Services -IncludeManagementTools
+Import-Module ADDSDeployment
+Install-ADDSForest `
+-CreateDnsDelegation:$false `
+-DatabasePath "C:\Windows\NTDS" `
+-DomainMode "Win2012R2" `
+-DomainName $DomainName `
+-DomainNetbiosName $DomainNetbiosName `
+-ForestMode "Win2012R2" `
+-InstallDns:$true `
+-LogPath "C:\Windows\NTDS" `
+-NoRebootOnCompletion:$true `
+-SysvolPath "C:\Windows\SYSVOL" `
+-Force:$true
+-SafeModeAdministratorPassword $SafeModeAdministratorPassword
